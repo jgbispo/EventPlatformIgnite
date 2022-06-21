ec5d1db0d000553d47626411f319fac3f9785196
João Gustavo Bispo <jgbispo20@gmail.com>
2022-06-21 20:22:47 -0300

    new font config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: 'Roboto, sans-serif'
+      },
         colors: {
           green: {
             300: '#00B37E',
